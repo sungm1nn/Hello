7cc109cdf5fd56f6c7fe7a40ee42622c615f81a0
hyeonmyeong <myeong58@cbnu.ac.kr>
2021-08-29 15:12:23 +0900

    googletrans API

---
+++ crawling/trans.py
@@ -0,0 +1,18 @@
+#pip install googletrans==4.0.0-rc1
+from googletrans import Translator
+
+
+def trans(text, transtext):
+    translator = Translator()
+    
+    for t in text:
+        la = translator.detect(text).lang
+        if len(t) > 5000:
+            a = [t[i:i + 5000] for i in range(0, len(t), 5000)]
+            for j in a:
+                trans_t += translator.translate(j, dest='ko').text
+        else:
+            trans_t= translator.translate(t, dest='ko').text
+        
+        transtext.append(trans_t)
+        print(trans_t)
