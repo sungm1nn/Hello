0cc2c8e410e25351815b34d97cd387ee4f945cc3
hyeonmyeong <myeong58@cbnu.ac.kr>
2021-09-03 16:38:09 +0900

    db test

---
+++ crawling/db.py
@@ -0,0 +1,30 @@
+import pymysql
+
+from russia import get_news, get_news_link
+
+key_title = []; key_text = []; key_update = []; key_img = []
+
+links = get_news_link()
+
+# for i in key_links:
+get_news(links[0], key_title, key_text, key_update, key_img)
+#print(links[0], key_title[0], key_text[0], key_update[0], key_img[0])
+
+conn = pymysql.connect(
+	host='113.198.137.163',
+	port=3306,
+	user='pnadm',
+	passwd='picknews1',
+	db='picknews'
+)
+
+c_code=643
+curs = conn.cursor()
+
+russia = "INSERT ignore INTO article_ledger (country_code, title, article_link, main_text, image_link, article_reg_dtime) VALUES (c_code, key_title[0],links[0],key_text[0],  key_img[0], key_update[0] )"
+
+Clear = "TRUNCATE TABLE article_ledger"
+
+curs.execute(Clear)
+
+print("AAA")
--- crawling/vietnam.py
+++ crawling/vietnam.py
@@ -2,6 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 import re
 from datetime import datetime
+from googletrans import Translator
 
 key = "covid"
 link = "https://tuoitre.vn/tim-kiem.htm?keywords="
@@ -11,9 +12,11 @@ key_title = []
 key_text = []
 key_update = []
 key_img = []
+trans_text = []
+num_news = 0
 
-def get_news_link(key):
-    URL = link + key
+def get_news_link():
+    URL = "https://tuoitre.vn/tin-moi-nhat.htm"
     req = requests.get(URL)
 
     soup = BeautifulSoup(req.text, 'html.parser')
@@ -26,28 +29,31 @@ def get_news_link(key):
     return key_links 
 
 
-def get_news(links,key_title, key_text, key_update,key_img):
+def get_news(num_news,links,key_title, key_text, key_update,key_img):
     URL = "https://tuoitre.vn/" + links
-    print(URL)
     req = requests.get(URL)
 
     soup = BeautifulSoup(req.text, 'html.parser')
 
-    title = soup.find(class_='article-title').text
-    key_title.append(title)
+    try:
+        title = soup.find(class_='article-title').text
+        key_title.append(title)
 
-    update = soup.find(class_='date-time').text
-    update = re.search(r'\d{2}/\d{2}/\d{4} \d{2}:\d{2}', update).group()
-    update = update.replace(" ", "/")
-    update = update.split('/')
-    update = update[2] + "-" + update[1] + "-" +  update[0] + " " +  update[3]
-    key_update.append(update)
+        update = soup.find(class_='date-time').text
+        update = re.search(r'\d{2}/\d{2}/\d{4} \d{2}:\d{2}', update).group()
+        update = update.replace(" ", "/")
+        update = update.split('/')
+        update = update[2] + "-" + update[1] + "-" +  update[0] + " " +  update[3]
+        key_update.append(update)
 
-    text_table = soup.find(class_='content fck')
-    text_list = text_table.find_all("p")
-    text = [li.get_text() for li in text_list]
-    text = ' '.join(text)
-    key_text.append(text)
+        text_table = soup.find(class_='content fck')
+        text_list = text_table.find_all("p")
+        text = [li.get_text() for li in text_list]
+        text = ' '.join(text)
+        key_text.append(text)
+        num_news += 1
+    except:
+        pass
 
     try:
         img = soup.find(class_="VCSortableInPreviewMode")
@@ -55,18 +61,38 @@ def get_news(links,key_title, key_text, key_update,key_img):
         key_img.append(img)
     except:
         key_img.append("no_img")
+    
+    return num_news
 
-key_links = get_news_link(key)
-# print(key_links[1])
-# get_news(key_links[1], key_title, key_text, key_update, key_img)
+def trans(text, transtext):
+    translator = Translator()
+    #translator = Translator(service_urls=['translate.googleapis.com'])
+    for t in text:
+        print(len(t))
+        if len(t) > 5000:
+            a = [t[i:i + 5000] for i in range(0, len(t), 5000)]
+            for j in a:
+                trans_t += translator.translate(j, src ='vi', dest='ko').text
+        else:
+            trans_t= translator.translate(t, src ='vi', dest='ko').text
+        
+        print(trans_t)
+        
+        #transtext.append(trans_t)
+
+
+key_links = get_news_link()
 
 for i in key_links:
-    get_news(i, key_title, key_text, key_update, key_img)
+    num_news =get_news(num_news,i, key_title, key_text, key_update, key_img)
 
-for i in range(len(key_links)):
+
+
+for i in range(num_news):
     print("link = " + key_links[i])
     print("title = " + key_title[i])
     print("update = " + key_update[i])
     print("text = " + key_text[i])
     print("img = " + key_img[i])
     print("\n\n")
+
