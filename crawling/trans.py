aab01b523601da1a762d951f0df3f0af7f84ee4b
sungm1nn <jjsm4690@naver.com>
2023-02-08 15:45:13 +0900

    update val

--- crawling/trans.py
+++ crawling/trans.py
@@ -1,14 +1,14 @@
 #pip install googletrans==4.0.0-rc1
 from googletrans import Translator
 
-
 def trans(text, transtext):
     translator = Translator()
     
     for t in text:
         la = translator.detect(text).lang
-        if len(t) > 5000:
-            a = [t[i:i + 5000] for i in range(0, len(t), 5000)]
+        tleng = len(t)
+        if tleng > 5000:
+            a = [t[i:i + 5000] for i in range(0, tleng, 5000)]
             for j in a:
                 trans_t += translator.translate(j, dest='ko').text
         else:
