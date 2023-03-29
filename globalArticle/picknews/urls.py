a7adc7f1f0a6905bb6c2eb988ec94be20926db40
sungm1nn <jjsm4690@naver.com>
2023-03-29 16:52:08 +0900

    page update

--- globalArticle/picknews/urls.py
+++ globalArticle/picknews/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = 'picknews'
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('subscription', views.subs, name='subs'),
 ]
--- globalArticle/picknews/views.py
+++ globalArticle/picknews/views.py
@@ -4,4 +4,7 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return render(request, 'base.html')
+
+def subs(request):
+    return render(request, 'picknews/subscription.html')
     #return HttpResponse("Hi")
