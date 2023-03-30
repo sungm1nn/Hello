d67bd278fa08449ff6d0a6ada5ef3ca5f345b9ab
sungm1nn <jjsm4690@naver.com>
2023-03-30 18:12:16 +0900

    update

--- globalArticle/picknews/models.py
+++ globalArticle/picknews/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class user_subs(models.Model):
+    user_id = models.CharField(max_length=200, primary_key=True, null=False)
+    keyword = models.CharField(max_length=50, null=False)
+    per = models.CharField(max_length=100, null=False)
+    nation = models.CharField(max_length=200 ,null=False) 
+    frequency = models.IntegerField()
+    none_check = models.BooleanField(default=False)
