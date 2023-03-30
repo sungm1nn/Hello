229a0ee0e84e3008cd9776aa9124113a07992b4f
sungm1nn <jjsm4690@naver.com>
2023-03-30 15:49:50 +0900

    update for log in - redirect

--- globalArticle/picknews/views.py
+++ globalArticle/picknews/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
+
 
 # Create your views here.
 def index(request):
     return render(request, 'base.html')
 
+@login_required(login_url='/common/login')
 def subs(request):
     return render(request, 'picknews/subscription.html')
     #return HttpResponse("Hi")
