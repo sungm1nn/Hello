12c41f08a3fe8d576887fafa982672827594963c
sungm1nn <jjsm4690@naver.com>
2023-04-06 17:48:09 +0900

    update

--- mysite/pybo/urls.py
+++ mysite/pybo/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
 
+app_name='pybo'
+
 urlpatterns = [
-    path('', views.index),
+    path('', views.index, name='index'),
+    path('<int:question_id>/', views.detail, name='detail'),
 ]
--- mysite/pybo/views.py
+++ mysite/pybo/views.py
@@ -1,10 +1,13 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
 from .models import Question
 
+# Create your views here.
 def index(request):
-    
     question_list = Question.objects.order_by('-create_date')
     context = {'question_list':question_list}
     return render(request, 'pybo/question_list.html', context)
-# Create your views here.
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id) #Question.objects.get(id=question_id)
+    context = {'question': question}
+    return render(request, 'pybo/question_detail.html', context)
