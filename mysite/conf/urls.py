83c4510596b4ec6a5072da5af624908f75dd11aa
sungm1nn <jjsm4690@naver.com>
2023-04-10 17:17:40 +0900

    update

--- mysite/conf/urls.py
+++ mysite/conf/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from pybo import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('pybo/', include('pybo.urls')),
+    path('', views.index, name='index'),
 ]
--- mysite/pybo/views.py
+++ mysite/pybo/views.py
@@ -1,11 +1,21 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Question
 from django.utils import timezone
+from .forms import QuestionForm, AnswerForm
+from django.core.paginator import Paginator
 
 # Create your views here.
 def index(request):
+    page = request.GET.get('page', '1')
+    
+    #조회
     question_list = Question.objects.order_by('-create_date')
-    context = {'question_list':question_list}
+    
+    #paging
+    paginator = Paginator(question_list, 10)
+    page_obj = paginator.get_page(page)
+    
+    context = {'question_list': page_obj}
     return render(request, 'pybo/question_list.html', context)
 
 def detail(request, question_id):
@@ -15,5 +25,28 @@ def detail(request, question_id):
 
 def answer_create(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
-    question.answer_set.create(content=request.POST.get('content'), create_date=timezone.now())
-    return redirect('pybo:detail', question_id=question.id)
+    if request.method == 'POST':
+        form = AnswerForm(request.POST)
+        if form.is_valid():
+            answer = form.save(commit=False)
+            answer.create_date = timezone.now()
+            answer.question = question
+            answer.save()
+            return redirect('pybo:detail', question_id=question.id)
+    else:
+        form = AnswerForm()
+    context = {'question':question ,'form':form}
+    return render(request, 'pybo/question_detail.html', context)
+
+def question_create(request):
+    if request.method == 'POST':
+        form = QuestionForm(request.POST)
+        if form.is_valid():
+            question = form.save(commit=False)
+            question.create_date = timezone.now()
+            question.save()
+            return redirect('pybo:index')
+    else:
+        form = QuestionForm()
+    context = {'form':form}
+    return render(request, 'pybo/question_form.html', context)
