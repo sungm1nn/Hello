2ae1708270fcb1f9b6343bcdd6204883c9c2b20e
sungm1nn <jjsm4690@naver.com>
2023-04-15 17:07:09 +0900

    commnt

--- mysite/pybo/models.py
+++ mysite/pybo/models.py
@@ -3,18 +3,20 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Question(models.Model):
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='author_question')
     subject = models.CharField(max_length=200)
     content = models.TextField()
     create_date = models.DateTimeField()
     modify_date = models.DateTimeField(null=True, blank=True)
+    voter = models.ManyToManyField(User, related_name='voter_question')
     
 class Answer(models.Model):
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='answer_answer')
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     content = models.TextField()
     create_date = models.DateTimeField()
     modify_date = models.DateTimeField(null=True, blank=True)
+    voter = models.ManyToManyField(User, related_name='voter_answer')
     
 class Comment(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
--- mysite/pybo/urls.py
+++ mysite/pybo/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import base_views, question_views, answer_views, comment_views
+from .views import base_views, question_views, answer_views, comment_views, vote_views
 # from . import views
 
 app_name='pybo'
@@ -26,6 +26,10 @@ urlpatterns = [
     path('comment/create/answer/<int:answer_id>/', comment_views.comment_create_answer, name='comment_create_answer'),
     path('comment/modify/answer/<int:comment_id>/', comment_views.comment_modify_answer, name='comment_modify_answer'),
     path('comment/delete/answer/<int:comment_id>/', comment_views.comment_delete_answer, name='comment_delete_answer'),
+    
+    #vote_views.py
+    path('vote/question/<int:question_id>/', vote_views.vote_question, name="vote_question"),
+    path('vote/answer/<int:answer_id>/', vote_views.vote_answer, name="vote_answer"),
 ]
 
 # urlpatterns = [
